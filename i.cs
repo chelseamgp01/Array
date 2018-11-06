b1a967d63770f5de6dc3fa4898d9ec88c89acf9d
chelseamgp01 <44809290+chelseamgp01@users.noreply.github.com>
2018-11-06 14:44:37 +0000

    Add files via upload

---
+++ i.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysEX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] student = new string[7];
+
+            student[0]= "Molly";
+            student[1]= "Sabba";
+            student[2]= "Hatty";
+            student[3]= "Sahra";
+            student[4]= "Jade";
+            student[5]= "Kitty";
+            student[6]= "Naomi";
+
+            for (int i = 0; i <=6; i++)
+            {
+                Console.WriteLine(student[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
