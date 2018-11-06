5c53df4907a9eff49ecc91593910cdc09fea2137
chelseamgp01 <44809290+chelseamgp01@users.noreply.github.com>
2018-11-06 14:43:22 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysEX2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Total = 0;
+            int[] numbers = new int[100];
+            Random random = new Random();
+            for (int i=0; i<=99; i++)
+            {
+                numbers[i] = random.Next(1, 100);
+                Console.WriteLine(numbers[i]);
+                Total = Total + numbers[i];
+               
+
+            }
+
+            Console.ReadLine();
+            Console.WriteLine(""+Total);
+        }
+    }
+}
